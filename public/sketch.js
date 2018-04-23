608dc7fd6d694557a29d1f6d938b2dd55b8e1b33
AKASH RAJ <akashraj9828@gmail.com>
2018-04-23 23:11:02 +0530

    mod

--- public/sketch.js
+++ public/sketch.js
@@ -5,7 +5,7 @@ function setup() {
   background(0);
   // Start a socket connection to the server
   // Some day we would run this server somewhere else
-  socket = io.connect('http://localhost:3000');
+  socket = io.connect('https://col-draw.herokuapp.com/');
   // We make a named event called 'mouse' and write an
   // anonymous callback function
   socket.on('mouse',
